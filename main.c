5c677c2e9b265c96365b1f8448011e376ccd5a89
saitoi <pedrohhs@dcc.ufrj.br>
2025-10-03 18:18:05 -0300

    add: named parameters (untested)

--- main.c
+++ main.c
@@ -52,8 +52,28 @@ int get_single_int(sqlite3 *db, const char *query) {
 
 int main(int argc, char *argv[]) {
     sqlite3 *db;
-    const char *filename = "busca.bin", *query_user;
+    const char *filename_tfidf = "tfidf.bin", *filename_db = "wiki-small.db";
+    const char *query_user = "exemplo";
     int nthreads;
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "--nthreads") == 0 && i + 1 < argc)
+            nthreads = atoi(argv[++i]);
+        else if (strcmp(argv[i], "--filename_db") == 0 && i + 1 < argc)
+            filename_db = argv[++i];
+        else if (strcmp(argv[i], "--filename_tfidf") == 0 && i + 1 < argc)
+            filename_tfidf = argv[++i];
+        else if (strcmp(argv[i], "--query_user") == 0 && i + 1 < argc)
+            query_user = argv[++i];
+        else {
+            fprintf(stderr, "Uso: %s <parametros nomeados>\n
+                            --nthreads: Número de threads (default: 4)\n
+                            --filename_db: Nome do arquivo Sqlite (default: 'wiki-small.db')\n
+                            --filename_tfidf: Nome do arquivo com estrutura do TF-IDF (default: 'tfidf.bin')\n
+                            --query_user: Consulta do usuário (default: 'exemplo')\n", argv[0]);
+            return 1;
+        }
+    }
 
     if (argc < 2) {
         fprintf(stderr, "Uso: %s <numero de threads> <texto>", argv[0]);
